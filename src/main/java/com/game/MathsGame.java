5e20474078bcca4ed8e6c496772e63115486e087
Nabakumar Luwang <nabakumar.l@gmail.com>
2020-01-30 23:09:10 +0000

    first commit of maths game

---
+++ src/main/java/com/game/MathsGame.java
@@ -0,0 +1,96 @@
+package com.game;
+
+import java.util.Collection;
+import java.util.function.BiFunction;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
+
+/**
+ * Generates two different types of collections as below from a given collection :
+ * 1) Applying isMultiple to each element of the given collection
+ * 2) Applying doMatch adn IsMultiple to each element of the given collection.
+ * @see isMultiple
+ * @see doMatch
+ */
+public class MathsGame {
+
+    private static final String FIZZ = "Fizz";
+    private static final String BUZZ = "Buzz";
+    /**
+     * Condition 1: The number is divisible by 3
+     * Condition 2: The number is divisible by 5
+     * Returns "FizzBuzz" if both conditions are met
+     * Returns "Fizz" if only Condition 1 is met.
+     * Returns "Buzz" if only Condition 2 is met.
+     */
+    protected Function<Integer, String> isMultiple =
+        input -> {
+            String fizzPart = (input % 3 == 0 ? FIZZ : "");
+            String buzzPart = (input % 5 == 0 ? BUZZ : "");
+            String result = fizzPart + buzzPart;
+            return "".equals(result) ? String.valueOf(input) : result;
+        };
+
+    /**
+     * Condition 1 : Input element contains "3" or equals "Fizz"
+     * Condition 2 : Input contains "5"  or equals "Buzz"
+     * Returns "FizzBuzz" if both conditions are met
+     * returns "Fizz" if only condition 1 is met
+     * returns "Buzz" if only condition 1 is met
+     * @see #isMultiple
+     */
+    protected BiFunction<Integer, Function<Integer, String>, String> doMatch =
+    (input,  isMultiple)-> {
+        String isMultipleResult = isMultiple.apply(input);
+        String inputStr = String.valueOf(input);
+        String fizzPart = (inputStr.contains("3") || isMultipleResult.contains(FIZZ)) ? FIZZ : "";
+        String buzzPart = (inputStr.contains("5") || isMultipleResult.contains(BUZZ)) ? BUZZ : "";
+        String result = fizzPart + buzzPart;
+        return "".equals(result) ? inputStr : result;
+    };
+
+    /**
+     * Applies isMultiple function on each element of the given stream.
+     * @param intStream
+     * @return resulting collection
+     * @see #isMultiple
+     */
+    public Collection<String> getMultiples(Stream<Integer> intStream){
+        return intStream.map(isMultiple).collect(Collectors.toList());
+    }
+
+    /**
+     * Applies doMatch function on each element of the given stream.
+     * @param intStream
+     * @return resulting collection
+     * @see #isMultiple
+     * @see #doMatch
+     */
+    public Collection<String> getMultiplesAndMatches(Stream<Integer> intStream){
+        return intStream.map(x -> doMatch.apply(x, isMultiple)).collect(Collectors.toList());
+    }
+
+    /**
+     * Prints the result of getMultiples() method with a stream of integers 1 to 100 both inclusive as input.
+     * @see #getMultiples(Stream<Integer>)
+     */
+    public void printMultiples(){
+        getMultiples(IntStream.range(1, 101).boxed()).forEach(System.out::println);
+    }
+
+    /**
+     * Prints the result of getMultiplesAndMatches() method with a stream of integers 1 to 100 both inclusive as input.
+     * @see #getMultiplesAndMatches(Stream<Integer>)
+     */
+    public void printMultiplesAndMatches(){
+        getMultiplesAndMatches(IntStream.range(1, 101).boxed()).forEach(System.out::println);
+    }
+
+    public static void main(String args[]){
+        MathsGame mg = new MathsGame();
+        mg.printMultiples();
+        //mg.printMultiplesAndMatches();
+    }
+}
---
+++ src/test/java/com/game/MathsGameTest.java
@@ -0,0 +1,142 @@
+package com.game;
+
+import org.junit.Test;
+
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+
+public class MathsGameTest {
+
+    private MathsGame gameUnderTest = new MathsGameUnderTest();
+    Stream<Integer> input = null;
+
+    // Start of areMultiples testcases
+    @Test
+    public void shouldReturnFizzWhenOnlyMultipleOf3(){
+        input = IntStream.of(9).boxed();
+        Collection result = gameUnderTest.getMultiples(input);
+        assertNotNull(result);
+        assertEquals(1, result.size());
+        assertEquals("Fizz", result.iterator().next());
+    }
+
+    @Test
+    public void shouldReturnBuzzWhenOnlyMultipleOf5(){
+        input = IntStream.of(10).boxed();
+        Collection result = gameUnderTest.getMultiples(input);
+        assertNotNull(result);
+        assertEquals(1, result.size());
+        assertEquals("Buzz", result.iterator().next());
+    }
+
+    @Test
+    public void shouldReturnFizzBuzzWhenMultipleOfBoth3And5(){
+        input = IntStream.of(15).boxed();
+        Collection result = gameUnderTest.getMultiples(input);
+        assertNotNull(result);
+        assertEquals(1, result.size());
+        assertEquals("FizzBuzz", result.iterator().next());
+    }
+
+    @Test
+    public void shouldReturnAsNumber(){
+        input = IntStream.of(4).boxed();
+        Collection result = gameUnderTest.getMultiples(input);
+        assertNotNull(result);
+        assertEquals(1, result.size());
+        assertEquals("4", result.iterator().next());
+    }
+
+    @Test
+    public void shouldReturnExpectedCollectionWithRandomInput(){
+        input = IntStream.of(1,2,5,6,7,30).boxed();
+        Collection result = gameUnderTest.getMultiples(input);
+        assertNotNull(result);
+        assertEquals(6, result.size());
+        Iterator<String> iter = result.iterator();
+        assertEquals("1", iter.next());
+        assertEquals("2", iter.next());
+        assertEquals("Buzz", iter.next());
+        assertEquals("Fizz", iter.next());
+        assertEquals("7", iter.next());
+        assertEquals("FizzBuzz", iter.next());
+    }
+    // End of Stage 1 (areMultiples) tests
+
+    // Start of Stage 2 (doMatch) tests
+    @Test
+    public void shouldReturnFizzWhenMultipleOf3OrWhenContains3OrBoth(){
+        input = IntStream.of(9,13,3).boxed();
+        Collection result = gameUnderTest.getMultiplesAndMatches(input);
+        assertNotNull(result);
+        assertEquals(3, result.size());
+        Iterator iter = result.iterator();
+        assertEquals("Fizz", iter.next());
+        assertEquals("Fizz", iter.next());
+        assertEquals("Fizz", iter.next());
+    }
+
+    @Test
+    public void shouldReturnBuzzWhenMultipleOf5OrWhenContains5OrBoth(){
+        input = IntStream.of(10,52,25).boxed();
+        Collection result = gameUnderTest.getMultiplesAndMatches(input);
+        assertNotNull(result);
+        assertEquals(3, result.size());
+        Iterator iter = result.iterator();
+        assertEquals("Buzz", iter.next());
+        assertEquals("Buzz", iter.next());
+        assertEquals("Buzz", iter.next());
+    }
+    @Test
+    public void shouldReturnFizzBuzzWhenMultipleOf3AndContains5(){
+        input = IntStream.of(51).boxed();
+        Collection result = gameUnderTest.getMultiplesAndMatches(input);
+        assertNotNull(result);
+        assertEquals(1, result.size());
+        Iterator iter = result.iterator();
+        assertEquals("FizzBuzz", iter.next());
+    }
+    @Test
+    public void shouldReturnFizzBuzzWhenMultipleOf5AndContains3(){
+        input = IntStream.of(35).boxed();
+        Collection result = gameUnderTest.getMultiplesAndMatches(input);
+        assertNotNull(result);
+        assertEquals(1, result.size());
+        Iterator iter = result.iterator();
+        assertEquals("FizzBuzz", iter.next());
+    }
+    @Test
+    public void shouldReturnNumberWhenNoCriteriaAreMet(){
+        input = IntStream.of(22).boxed();
+        Collection result = gameUnderTest.getMultiplesAndMatches(input);
+        assertNotNull(result);
+        assertEquals(1, result.size());
+        Iterator iter = result.iterator();
+        assertEquals("22", iter.next());
+    }
+
+    @Test
+    public void shouldReturnExpectedResultsWithRandomInput(){
+        input = IntStream.of(1,2,5,7,13,53).boxed();
+        Collection result = gameUnderTest.getMultiplesAndMatches(input);
+        assertNotNull(result);
+        assertEquals(6, result.size());
+        Iterator iter = result.iterator();
+        assertEquals("1", iter.next());
+        assertEquals("2", iter.next());
+        assertEquals("Buzz", iter.next());
+        assertEquals("7", iter.next());
+        assertEquals("Fizz", iter.next());
+        assertEquals("FizzBuzz", iter.next());
+    }
+    // End of Stage 2 (doMatch) tests
+}
+
+class MathsGameUnderTest extends MathsGame{
+
+}
